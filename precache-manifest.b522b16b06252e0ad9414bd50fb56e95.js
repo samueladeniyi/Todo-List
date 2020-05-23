f956153e6ee0101808521910b03948d2cc7fd6c9
samuel <newtumi@gmail.com>
2020-05-23 17:51:39 +0100

    first commit

---
+++ precache-manifest.b522b16b06252e0ad9414bd50fb56e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa6fd94a960fbc54c70d755ade66c72",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "093e0bbac3b5edce33b1",
+    "url": "/react/static/js/2.aed278ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.aed278ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bf88fea484f3b4673a",
+    "url": "/react/static/js/main.7cd1ce7f.chunk.js"
+  },
+  {
+    "revision": "e67e5fbf715d04e0600c",
+    "url": "/react/static/js/runtime-main.2042c41c.js"
+  }
+]);
